77cda5246a56fe3fb4120bb281efe61c9a0e02d3
L. S. Junior <90662061+LsTheJunior@users.noreply.github.com>
2022-01-10 23:58:44 -0300

    Add files via upload

---
+++ ETAPA3_LOAD_API_REST.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, jsonify, request
+from pydantic import BaseModel
+from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
+from tinydb import TinyDB, database, Query
+from typing import Any, Optional
+
+
+server = Flask(__name__)
+spec = FlaskPydanticSpec('flask', title='API Desafio Cross Commerce')
+spec.register(server)
+database = TinyDB(r'C:\Users\Public\database.json')
+
+
+class lista(BaseModel):
+    resulatdo: list
+
+@server.get('/ListaOrdenada')
+@spec.validate(resp=Response(HTTP_200=lista))  
+def buscar_pessoas():
+    """Retorna a lista ordenada obtida na etapa 2 - Transform"""
+    
+    return jsonify(database.all())
+
+
+
+
+server.run()
